9327ff01b01bed0cdde552497790cd85820e9ac6
Veggieee <jungjaroen6879@gmail.com>
2024-08-13 08:36:49 +0700

    typo

--- 02-low-level-file-access/charCopy.c
+++ 02-low-level-file-access/charCopy.c
@@ -8,7 +8,7 @@ int main(void){
   char c;
   int in, out;
 
-  in = open("file.in", O_WRONLY);
+  in = open("file.in", O_RDONLY);
   out = open("file.out", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
   while(read(in, &c, 1) == 1) write(out, &c, 1);
 
